e8f7195dc3ee5aaa5960b6b8745128297c181d87
Deyvid-Rian <deyvidrian68@gmail.com>
2022-01-16 23:32:40 -0300

    att 16/01/2022

---
+++ javascript/aula.js
@@ -0,0 +1,45 @@
+//declarando uma variável de 
+var preco = 54.90;
+
+//multilicando um numero
+var desconto = preco * 0.1;
+
+//imprimindo um valor
+console.log("O valor do desconto é:" + desconto)
+
+var aluno = "Joaquim"; //Variável do tipo string
+
+aluno = {   //variável do tipo object
+    nome: "João",
+    idade: 20
+};
+
+//Variável do tipo number
+var valorDoServico = 20.00;
+
+//Variável do tipo array
+valorDoServico = [13.99, 34.87, 29.99];
+
+//Exemplo de case sensitive
+
+var nomeusuario = "João";
+var nomeUsuario = "Tiago";
+
+console.log(nomeusuario);
+//João
+
+console.log(nomeUsuario);
+//Tiago
+
+//////////////////////////////////////////////
+
+
+var primeiroNumero = 9;
+var segundoNumero = 4;
+console.log(primeiroNumero + segundoNumero);
+
+//Testes
+var idade = 20;
+idade = 20 + " anos";
+
+console.log(idade);
